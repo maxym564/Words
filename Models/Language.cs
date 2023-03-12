44a6521791d96747e4e9dd08a284b18d6bc9603a
Maksym <seredovichm@gmail.com>
2023-03-12 23:46:56 +0200

    fixed models by g1

--- Models/Language.cs
+++ Models/Language.cs
@@ -11,7 +11,7 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-        public string Description { get; set; }
+        public String Description { get; set; }
 
         public Language(Guid id, string title, List<Word> words, string description)
         {
@@ -24,7 +24,6 @@ namespace words.Models
         public double getAverageLevel()
         {
             return Words.Sum(x => x.Level) / Words.Count;
-
         }
 
         public override string ToString()
